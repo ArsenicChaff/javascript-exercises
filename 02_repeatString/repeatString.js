de8c3eb3a24d4fba0589811f0a385b6fd480f6da
ArsenicChaff <163602000+ArsenicChaff@users.noreply.github.com>
2024-04-12 16:22:08 -0400

    Complete part 1 of assignment 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let tempOutput = ''
+    
+    for (i = 0; i < num; i++){
+        tempOutput += string;
+    }
+    console.log(tempOutput);
 };
+
+// console.log(repeatString('hey', 9));
 
 // Do not edit below this line
 module.exports = repeatString;
