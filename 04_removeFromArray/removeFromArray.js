22092eb5a23660e7e7a9ef76ec23af6d2925896d
ArsenicChaff <163602000+ArsenicChaff@users.noreply.github.com>
2024-04-13 18:29:11 -0400

    Complete 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,36 @@
+// const removeFromArray = function(array, ...numbers) {
+//     let charInputArray = numbers;
+//     let posIfInArray = 1;
+//     for (i = 0, n = charInputArray.length; i < n; i++) {
+//         posIfInArray = array.indexOf(charInputArray[i]);
+//         while (posIfInArray != -1) {
+//             if (posIfInArray === -1) {
+//                 return;
+//             } else {
+//             array.splice(posIfInArray, 1)
+//             posIfInArray = array.indexOf(charInputArray[i]);
+//             // console.log(posIfInArray);
+//             }
+//         }
+//     return array;
+//     }
+// };
+
 const removeFromArray = function(array, ...numbers) {
+    let lenInputArray = array.length;
+    // console.log(lenInputArray);
     let charInputArray = numbers;
+    // console.log(charInputArray);
+    let posIfInArray = 99;
     for (i = 0, n = charInputArray.length; i < n; i++) {
-        let valueByPosition = charInputArray[i]
-        let posIfInArray = array.indexOf(valueByPosition);
+        posIfInArray = array.indexOf(charInputArray[i]);  //checks if the value is in the input array
+        console.log(posIfInArray);   //stores positional index in reference to input array
         if (posIfInArray > -1) {
             array.splice(posIfInArray, 1)
+            let check = array.indexOf(charInputArray[i]);
+            if (check > -1) {
+                array.splice(posIfInArray, 1)
+            }
         }
     }
     return array;
--- 04_removeFromArray/test.js
+++ 04_removeFromArray/test.js
@@ -3,16 +3,18 @@ const removeFromArray = function(array, ...numbers) {
     // console.log(lenInputArray);
     let charInputArray = numbers;
     // console.log(charInputArray);
-
+    let posIfInArray = 99;
     for (i = 0, n = charInputArray.length; i < n; i++) {
-        let valueByPosition = charInputArray[i]
-        console.log(valueByPosition);
-        let posIfInArray = array.indexOf(valueByPosition);  //checks if the value is in the input array
+        posIfInArray = array.indexOf(charInputArray[i]);  //checks if the value is in the input array
         console.log(posIfInArray);   //stores positional index in reference to input array
         if (posIfInArray > -1) {
             array.splice(posIfInArray, 1)
+            let check = array.indexOf(charInputArray[i]);
+            if (check > -1) {
+                array.splice(posIfInArray, 1)
+            }
         }
     }
     console.log(array);
 };
-removeFromArray([1, 2, 3], 2);
+removeFromArray([1, 2, 2, 4, 6, 2, 3, 3], 2, 1, 6);
