943f9d5da275a9617a25a8b8a8e30e99909821a6
ArsenicChaff <163602000+ArsenicChaff@users.noreply.github.com>
2024-04-14 00:00:58 -0400

    Tidy code, revision for simplicity

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,25 +5,23 @@ let finalSum = 0;
 let largerInt;
 let smallerInt;
 
-if ((intOne < 0) || (intTwo < 0)) {
-    return 'ERROR';
-} else if (((typeof intOne) != 'number') || ((typeof intTwo) != 'number')) {
-    //check to make sure intOne and intTwo are integers || number operators
-    return 'ERROR';
+if ((intOne < 0) || (intTwo < 0)) return 'ERROR';
+if (((typeof intOne) != 'number') || ((typeof intTwo) != 'number')) return 'ERROR';
+
+if (intOne < intTwo) {
+    smallerInt = intOne;
+    largerInt = intTwo;
 } else {
-    if (intOne < intTwo) {
-        smallerInt = intOne;
-        largerInt = intTwo;
-    } else {
-        smallerInt = intTwo;
-        largerInt = intOne;
-    }
-    for (i = smallerInt; i <= largerInt; i++){
-        finalSum += i;
-    }
-    return finalSum;
-    };
+    smallerInt = intTwo;
+    largerInt = intOne;
 }
+
+for (i = smallerInt; i <= largerInt; i++){
+    finalSum += i;
+}
+return finalSum;
+};
+
 
 // Do not edit below this line
 module.exports = sumAll;
