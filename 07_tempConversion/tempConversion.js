614b8e33bd4e5b21bc840d9c60dc229f1c7758a5
ArsenicChaff <163602000+ArsenicChaff@users.noreply.github.com>
2024-04-14 00:35:07 -0400

    Complete all tests 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+//    C = (F - 32) ÷ 9/5
+//    F = (C × 9/5) + 32
+
+
+const convertToCelsius = function(value) {
+  return Math.round(((value - 32) / (9/5)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  return Math.round(((value * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
