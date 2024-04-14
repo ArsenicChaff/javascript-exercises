eba4b7b1ae2188fb14dd426cfac10fa51b8f98e2
ArsenicChaff <163602000+ArsenicChaff@users.noreply.github.com>
2024-04-13 23:39:33 -0400

    Pass test 1-2 of 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+// Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
+let finalSum = 0;
+for (i = intOne; i <= intTwo; i++){
+    finalSum += i;
+}
+return finalSum;
 };
+
+
+// sumAll(1, 5);
+
 
 // Do not edit below this line
 module.exports = sumAll;
